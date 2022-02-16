af44fa92035f4be3d9ca0a5e78202c746f9a4b3a
evandiewald <37876940+evandiewald@users.noreply.github.com>
2022-02-16 12:26:10 -0500

    added TTL cache

--- server.py
+++ server.py
@@ -1,30 +1,38 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 import boto3
+from cachetools import cached, TTLCache
+
 
 S3_BUCKET = "dewi-etl-data-dumps"
 URL_PREFIX = f"https://{S3_BUCKET}.s3.amazonaws.com/"
 
 app = FastAPI()
 client = boto3.client("s3")
-folders = [f["Prefix"] for f in client.list_objects(Bucket=S3_BUCKET, Delimiter="/")["CommonPrefixes"]]
-objects_cache = {}
-for folder in folders:
-    files = [f for f in client.list_objects(Bucket=S3_BUCKET, Prefix=folder)["Contents"]]
-    objects_cache[folder[:-1]] = files
-
 templates = Jinja2Templates("templates")
+
+
+@cached(TTLCache(maxsize=1, ttl=600))
+def get_objects():
+    folders = [f["Prefix"] for f in client.list_objects(Bucket=S3_BUCKET, Delimiter="/")["CommonPrefixes"]]
+    folders_cache = {}
+    for folder in folders:
+        files = [f for f in client.list_objects(Bucket=S3_BUCKET, Prefix=folder)["Contents"]]
+        folders_cache[folder[:-1]] = files
+    return folders, folders_cache
 
 
 @app.get("/")
 async def homepage(request: Request):
+    folders, folders_cache = get_objects()
     return templates.TemplateResponse("index.html", {"request": request, "folders": folders})
 
 
-@app.get("/{folder}")
+@app.get("/folder/{folder}/")
 async def files_in_folder(request: Request, folder: str):
-    return templates.TemplateResponse("folder.html", {"request": request, "folder": folder, "url_prefix": URL_PREFIX, "files": objects_cache[folder]})
-
+    folders, folders_cache = get_objects()
+    files = folders_cache[folder]
+    return templates.TemplateResponse("folder.html", {"request": request, "folder": folder, "url_prefix": URL_PREFIX, "files": files})
 
 
 
